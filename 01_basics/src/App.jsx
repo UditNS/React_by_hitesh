ead17d82ee044af5b82f8b847b3c4a54f97d38c2
Udit Narayan Singh <uditn007@gmail.com>
2025-08-26 16:55:50 +0530

    Understand the react flow and structure. Created new component.

---
+++ 01_basics/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Test from "./test"
+
+function App() {
+  return (
+    <>
+      <h1>hello dude </h1>
+      <Test />
+    </>
+  )
+}
+
+export default App
---
+++ 01_basics/src/Test.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function Test(){
+    return (
+        <h1>Creating component, this is a test component</h1>
+    )
+}
+
+export default Test
