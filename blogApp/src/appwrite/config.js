51e53ee3fd642012446d49ee4057ae167e83f5cf
Udit Narayan Singh <uditn007@gmail.com>
2025-09-04 13:14:22 +0530

    added file upload service

--- blogApp/src/appwrite/config.js
+++ blogApp/src/appwrite/config.js
@@ -87,7 +87,18 @@ export class Service{
             return false;
         }
     }
-    
+    // file upload service
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("error in uploadFile" + error)
+            return false;
+        }
 }
 
 const service = new Service();
