ea3b24b006dd811633026f4a9ea95103a3af6c23
Udit Narayan Singh <uditn007@gmail.com>
2025-09-04 12:51:06 +0530

    Imported the necessary method from appwrite.
    Implemented the createPost function.

---
+++ blogApp/src/appwrite/config.js
@@ -0,0 +1,35 @@
+import conf from "../conf/conf";
+import { Client, Account, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service{
+    client = new Client;
+    databases;
+    bucket;
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl) 
+        .setProject(conf.appwriteProjectId);  
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+    
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        return await this.databases.createDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug,  // unique id
+            {
+                title,
+                content,
+                featuredImage,
+                status,
+                userId
+            }
+        )
+
+    }
+}
+
+const service = new Service
+
+export default service;
